f82329c99903229988618e63cfa31b5d69da6a6a
qufnr <qufnr@kakao.com>
2025-12-17 13:02:43 +0900

    added: 계정 아이콘 컴포넌트 추가

---
+++ src/components/BMemberAvatarIcon.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import type { Member } from '@/composables/api/member/types.ts'
+
+type BMemberAvatarIconProps = {
+    member: Member
+    size?: number | string
+}
+
+const { member, size } = defineProps<BMemberAvatarIconProps>()
+
+//  이름 이니셜
+const nameInitial = computed<string>(() => {
+    if(member == null)
+        return ''
+
+    return member.name != null
+        ? member.name.charAt(0).toUpperCase()
+        : member.id.charAt(0).toUpperCase()
+})
+</script>
+
+<template>
+    <v-avatar :color="member.colour" :size="size">
+        <v-img v-if="member.avatar != null"
+               :src="member.avatar"
+        />
+        <p>{{ nameInitial }}</p>
+    </v-avatar>
+</template>
+
+<style scoped>
+
+</style>
