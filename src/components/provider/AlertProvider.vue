a2f5799e564b9cdeeb1d281ff795cf0b80009c3a
qufnr <qufnr@kakao.com>
2026-01-08 11:49:30 +0900

    modified: Alert Provider 에서 Alert 이 닫힌 후 초기화 되는 시점에 텍스트가 사라지는게 보이는 문제 해결

--- src/components/provider/AlertProvider.vue
+++ src/components/provider/AlertProvider.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
 import { useAlertStore } from '@/stores/alert'
+import { TimeUtils } from '@/utils/time'
 
 //  Alert Store
 const alertStore = useAlertStore()
@@ -9,8 +10,10 @@ const { visible, title, message, options } = storeToRefs(alertStore)
 /**
  * Alert 닫히면 초기화
  */
-function onAlertClose() {
-    console.log('is alert close?')
+async function onAlertClose() {
+    //  트렌지션 때문에 진짜 조금있다가 초기화
+    await TimeUtils.sleep(.1)
+
     title.value = undefined
     message.value = undefined
     options.value = alertStore.defaultOptions
