5cbdd7e1d9ebb08f41faab3b7df1630c45360c70
qufnr <qufnr@kakao.com>
2026-02-25 17:54:36 +0900

    added: 알림 서비스 추가

---
+++ src/services/notification/index.ts
@@ -0,0 +1,42 @@
+import http from '@/http'
+import type { PaginationResponse } from '@/http/types.ts'
+import type {
+    NotificationDeleteRequest,
+    NotificationReadRequest,
+    NotificationResponse,
+    NotificationUpdateRequest
+} from '@/services/notification/types.ts'
+
+export const NotificationService = {
+    /**
+     * 알림 조회
+     *
+     * @param request 요청 정보
+     * @return 페이지 응답
+     */
+    read: async (request: NotificationReadRequest) =>
+        (await http.get<PaginationResponse<NotificationResponse>>(`notification-management/notifications`, { params: request })).data,
+
+    /**
+     * 알림 수정 (읽음 처리)
+     *
+     * @param uid 알림 UID
+     * @param request 요청 정보
+     */
+    update: async (uid: number, request: NotificationUpdateRequest) => await http.put(`notification-management/notifications/${uid}`, { params: request }),
+
+    /**
+     * 알림 단건 삭제
+     *
+     * @param uid 알림 UID
+     */
+    delete: async (uid: number) => await http.delete(`notification-management/notifications/${uid}`),
+
+    /**
+     * 알림 일괄 삭제
+     *
+     * @param request 요청 정보
+     */
+    deleteAll: async (request: NotificationDeleteRequest) =>
+        await http.delete('notification-management/notifications', { params: request })
+}
---
+++ src/services/notification/types.ts
@@ -0,0 +1,38 @@
+import type { ShortMember } from '@/services/member/types.ts'
+
+//  알림 유형
+export type NotificationType =
+    'FOLLOW' |
+    'FOLLOW_REQUEST' |
+    'GUILD_MENTION' |
+    'SYSTEM'
+
+//  알림 조회 요청
+export interface NotificationReadRequest {
+    page: number
+    size: number
+    isRead?: boolean | null
+    standardDate?: string | null
+}
+
+//  알림 수정 요청
+export interface NotificationUpdateRequest {
+    isRead: boolean
+}
+
+//  알림 삭제 요청 (일괄 삭제)
+export interface NotificationDeleteRequest {
+    uids?: number[]
+    onlyRead: boolean
+}
+
+//  알림 응답
+export interface NotificationResponse {
+    uid: number
+    receiver: ShortMember
+    sender?: ShortMember | null
+    message: string
+    type: NotificationType
+    isRead: boolean
+    sentAt: string
+}
