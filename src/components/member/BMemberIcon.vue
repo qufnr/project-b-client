89ec94aac2b065ec96454e456a7b2b2171e85db1
qufnr <qufnr@kakao.com>
2026-02-26 16:12:39 +0900

    refactor: BMemberIcon 컴포넌트 수정
    - `member` 속성 nullable 로 변경
    - `avatarSrc` Getter 에서 `member` 속성 null 체크
    - `avatarColour` Getter 추가

--- src/components/member/BMemberIcon.vue
+++ src/components/member/BMemberIcon.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
 import { computed } from 'vue'
-import type { Member } from '@/services/member/types.ts'
+import type { Member, ShortMember } from '@/services/member/types.ts'
 
 interface BMemberAvatarIconProps {
-    member: Member
+    member?: Member | ShortMember
     src?: string
     size?: number | string
+    system?: boolean
+    colour?: string
 }
 
 //  Props
-const { member, src, size } = defineProps<BMemberAvatarIconProps>()
+const { member, src, size, colour, system = false } = defineProps<BMemberAvatarIconProps>()
 
 //  서버 정적 리소스 URL
 const serverStaticUrl = import.meta.env.VITE_APP_SERVER_STATIC_URL
@@ -29,17 +31,28 @@ const nameInitialSize = computed(() => `${Number(size) / 2}px`)
 
 //  Src
 const avatarSrc = computed(() => {
-    if(member.avatar != null)
+    if(member != null && member.avatar != null)
         return member.avatar.startsWith('blob:') ?
             member.avatar :
             `${serverStaticUrl}avatar/${member.avatar}`
 
     return null
 })
+
+//  Colour
+const avatarColour = computed(() => {
+    if(member != null && member.avatar != null)
+        return undefined
+
+    if(member != null && member.avatar == null)
+        return member.colour
+
+    return colour
+})
 </script>
 
 <template>
-    <v-avatar :color="member.avatar != null ? undefined : member.colour" :size="size" class="select-none">
+    <v-avatar :color="avatarColour" :size="size" class="select-none">
         <v-img v-if="avatarSrc != null" :src="avatarSrc" />
         <v-img v-else-if="!!src" :src="src" />
         <p v-else :style="{ fontSize: nameInitialSize }">{{ nameInitial }}</p>
